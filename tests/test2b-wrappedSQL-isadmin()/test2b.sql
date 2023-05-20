6e3af10ee00b78d1c640e0ce6d6fb3ac66c66b91
GaryAustin1 <54564956+GaryAustin1@users.noreply.github.com>
2023-05-20 16:32:45 -0500

    Update test2b.sql

--- tests/test2b-wrappedSQL-isadmin()/test2b.sql
+++ tests/test2b-wrappedSQL-isadmin()/test2b.sql
@@ -14,7 +14,7 @@ create table
     rlstest_roles as
 select x as id, 'name-' || x as name, uuid_generate_v4() as user_id, 'user' as role
 from generate_series(1, 1000) x;
-update rlstest_roles set (user_id,role) = ('70225db6-b0ba-4116-9b08-6b25f33bb70a','admin') where id = 1;
+update rlstest_roles set (user_id,role) = ('70225db6-b0ba-4116-9b08-6b25f33bb70a','user') where id = 1;
 alter table rlstest_roles ENABLE ROW LEVEL SECURITY;
 
 CREATE OR REPLACE FUNCTION is_admin()
@@ -51,11 +51,13 @@ Execution Time: 11578.815 ms  <<<<!!!!!!   11 seconds, 100kx1000 potential itera
 
 With select(is_admin()) as RLS:
 
-Aggregate  (cost=1959.26..1959.27 rows=1 width=8) (actual time=15.325..15.326 rows=1 loops=1)
+Aggregate  (cost=1509.80..1509.81 rows=1 width=8) (actual time=7.387..7.388 rows=1 loops=1)
   InitPlan 1 (returns $0)
-    ->  Result  (cost=0.00..0.26 rows=1 width=1) (actual time=0.497..0.498 rows=1 loops=1)
-  ->  Seq Scan on rlstest  (cost=0.00..1834.00 rows=50000 width=0) (actual time=0.506..9.577 rows=100000 loops=1)
+    ->  Result  (cost=0.00..0.26 rows=1 width=1) (actual time=0.520..0.521 rows=1 loops=1)
+  ->  Seq Scan on rlstest  (cost=0.00..1434.48 rows=30024 width=0) (actual time=7.380..7.380 rows=0 loops=1)
         Filter: $0
-Planning Time: 0.091 ms
-Execution Time: 15.366 ms
+        Rows Removed by Filter: 100000
+Planning Time: 0.099 ms
+Execution Time: 7.429 ms
+
 */
