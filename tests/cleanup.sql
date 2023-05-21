0cedb1598d389bd0b5e759bf460f01ee325d3fa5
GaryAustin1 <54564956+GaryAustin1@users.noreply.github.com>
2023-05-21 17:34:13 -0500

    Create cleanup.sql

---
+++ tests/cleanup.sql
@@ -0,0 +1,5 @@
+drop table if exists rlstest;
+drop table if exists rlstest_roles;
+drop table if exists rlstest_team_user;
+drop function if exists rlstest_is_admin;
+drop function if exists rlstest_user_teams;
