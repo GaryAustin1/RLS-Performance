c2368acf2d21e36bec3dac552747098116afbb9e
GaryAustin1 <54564956+GaryAustin1@users.noreply.github.com>
2023-05-30 13:15:57 -0500

    Update cleanup.sql

--- tests/cleanup.sql
+++ tests/cleanup.sql
@@ -3,3 +3,4 @@ drop table if exists rlstest_roles;
 drop table if exists rlstest_team_user;
 drop function if exists rlstest_is_admin;
 drop function if exists rlstest_user_teams;
+drop function if exists get_role();
