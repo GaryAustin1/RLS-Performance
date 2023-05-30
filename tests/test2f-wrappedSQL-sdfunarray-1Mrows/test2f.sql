1b1dcde3aa23f4c22f370a6f8655872aabb08011
GaryAustin1 <54564956+GaryAustin1@users.noreply.github.com>
2023-05-30 17:30:34 -0500

    Update test2f.sql

--- tests/test2f-wrappedSQL-sdfunarray-1Mrows/test2f.sql
+++ tests/test2f-wrappedSQL-sdfunarray-1Mrows/test2f.sql
@@ -1,1 +1,81 @@
+-- Testing wrapping SQL around a security definer function getting role based on auth.uid() from a 2nd table.
+-- Change using() between commented policies 1MILLON ROWS
+
+drop table if exists rlstest;
+create table
+    rlstest as
+select x as id, 'name-' || x as name, uuid_generate_v4() as user_id, 'user' as role, x as team_id
+from generate_series(1, 1000000) x;
+alter table rlstest ENABLE ROW LEVEL SECURITY;
+
+create index team on rlstest using btree (team_id) tablespace pg_default;
+
+drop table if exists rlstest_team_user;
+create table
+    rlstest_team_user as
+select x as id, 'name-' || x as name, uuid_generate_v4() as user_id,x as team_id
+from generate_series(1, 1000) x;
+update rlstest_team_user set (user_id,team_id) = ('70225db6-b0ba-4116-9b08-6b25f33bb70a',1) where id <50 ;
+update rlstest_team_user set (user_id,team_id) = ('70225db6-b0ba-4116-9b08-6b25f33bb70a',2) where id >50 and id <100;
+
+
+alter table rlstest_team_user ENABLE ROW LEVEL SECURITY;
+create policy "dummy_read_policy" on rlstest_team_user
+    to authenticated using (auth.uid() = user_id );
+
+CREATE OR REPLACE FUNCTION rlstest_user_teams()
+    RETURNS int[] as
+$$
+begin
+    return array( select team_id from rlstest_team_user where auth.uid() = user_id);
+end;
+$$ language plpgsql security definer;
+drop policy rls_test_select on rlstest;
+create policy "rls_test_select" on rlstest
+    to authenticated
+    using (
+    --    team_id in (1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100)
+   team_id = any ( array(select rlstest_user_teams()))
+   --team_id = any(rlstest_user_teams())
+    );
+
+set session role authenticated;
+set request.jwt.claims to '{"role":"authenticated", "sub":"70225db6-b0ba-4116-9b08-6b25f33bb70a"}';
+
+explain analyze SELECT * FROM rlstest;
+
+set session role postgres;
+
+/*
+Hardcoding:100teams
+Gather  (cost=1000.25..19078.68 rows=99 width=40) (actual time=0.282..142.843 rows=99 loops=1)
+  Workers Planned: 1
+  Workers Launched: 0
+  ->  Parallel Seq Scan on rlstest  (cost=0.25..18068.78 rows=58 width=40) (actual time=0.053..142.518 rows=99 loops=1)
+"        Filter: (team_id = ANY ('{1,2,3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100}'::integer[]))"
+        Rows Removed by Filter: 999901
+Planning Time: 0.163 ms
+Execution Time: 142.872 ms
+
+100teams
+Seq Scan on rlstest  (cost=0.26..31745.26 rows=10 width=40) (actual time=2.136..721.356 rows=2 loops=1)
+  Filter: (team_id = ANY ($0))
+  Rows Removed by Filter: 999998
+  InitPlan 1 (returns $0)
+    ->  Result  (cost=0.00..0.26 rows=1 width=32) (actual time=2.092..2.092 rows=1 loops=1)
+Planning Time: 0.093 ms
+Execution Time: 721.392 ms
+
+100 teams indexed...
+Bitmap Heap Scan on rlstest  (cost=534.11..10879.13 rows=48890 width=88) (actual time=3.147..3.150 rows=2 loops=1)
+  Recheck Cond: (team_id = ANY ($0))
+  Heap Blocks: exact=1
+  InitPlan 1 (returns $0)
+    ->  Result  (cost=0.00..0.26 rows=1 width=32) (actual time=2.528..2.528 rows=1 loops=1)
+  ->  Bitmap Index Scan on team  (cost=0.00..521.62 rows=48890 width=0) (actual time=3.128..3.128 rows=2 loops=1)
+        Index Cond: (team_id = ANY ($0))
+Planning Time: 0.939 ms
+Execution Time: 3.189 ms
+
+*/
 
